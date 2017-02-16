24a33a1f46de6e37b8804c0922b335ea30f9b193
Vincent Martinez <vincent@strict.so>
2017-02-16 15:35:16 +0000

    Enable dependency injection

--- functions/email_tracker/index.js
+++ functions/email_tracker/index.js
@@ -7,7 +7,7 @@ exports.handle = function handle(e, ctx) {
     headers: { location: 'https://s3.amazonaws.com/s.sumofus.org/pixel.gif' },
   };
 
-  const tracker = new OpenTracker();
+  const tracker = ctx.tracker || new OpenTracker();
   tracker.track({ mailing_id: query.mailing_id, user_id: query.user_id })
     .then((data) => {
       console.log('Successfully tracked:', query, data);
