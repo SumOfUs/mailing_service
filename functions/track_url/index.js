7cc19db4699f1e72c5f3317482270c40c0935a0f
Vincent Martinez <vincent@strict.so>
2017-02-16 18:44:06 +0000

    Add track_url (WIP)

---
+++ functions/track_url/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const UrlTracker = require('./url-tracker').UrlTracker;
+
+exports.handle = function handle(e, ctx) {
+  const query = e.queryStringParameters;
+  const response = {
+    statusCode: 302,
+    headers: { location: query.url },
+  };
+
+  const tracker = ctx.tracker || new UrlTracker();
+  tracker.track({ mailing_id: query.mailing_id, user_id: query.user_id })
+    .then((data) => console.log('Successfully tracked url:', query, data),
+      (error) => console.log('there was an error:', error, query))
+    .then(() => ctx.succeed(response));
+};
