c0386cefa1fc22a1b4abbada1ec853221eaaee17
Omar Sahyoun <osahyoun@gmail.com>
2018-06-01 14:54:22 +0100

    Send one email per recipient

--- functions/mail/index.js
+++ functions/mail/index.js
@@ -3,42 +3,42 @@
 const builder = require('./email_options_builder');
 const sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
-const json = (data) => {
-  JSON.stringify(data, null, 2)
-};
-
-const sendEmail = (opts) => {
+const sendEmail = opts => {
   let options = builder(opts);
   let req = sendgrid.emptyRequest(options);
 
-  sendgrid.API(req).
-    then((response) => {
-      console.log('SENDGRID::SUCCESS', json(response), json(options));
-    }).catch((err) => {
-      console.log('SENDGRID::ERROR', json(err), json(options));
-    });
+  sendgrid.API(req)
+    .catch( err => console.log(err.response.body));
 };
 
-const processRecords = (events) => {
-  events.forEach( (record) => {
-    if(record.eventName !== 'INSERT') return;
-    let data = record.dynamodb.NewImage;
+const processEvent = record => {
+  const data = record.dynamodb.NewImage;
 
-    let emailOptions = {
-      body: data.Body.S,
-      subject: (data.Subject || {}).S,
-      toEmailAddresses: data.ToEmails.S,
-      ccEmailAddresses: (data.CCEmails || {}).S,
-      fromEmailAddress: data.FromEmail.S,
-      fromName: data.FromName.S,
-      page: data.MailingId.S.split(':')[0],
-    };
+  const emailOptions = {
+    body: data.Body.S,
+    subject: (data.Subject || {}).S,
+    fromEmailAddress: data.FromEmail.S,
+    fromName: data.FromName.S,
+    page: data.MailingId.S.split(':')[0],
+  };
 
+  const toEmails = data.ToEmails.L;
+
+  for (let contact of toEmails) {
+    emailOptions.toEmailAddress = contact.S;
     sendEmail(emailOptions);
-  })
-}
+  };
+};
 
 module.exports.handler = (event, content, callback) => {
-  processRecords(event.Records);
+  const record = event.Records[0];
+  if(record.eventName === 'INSERT') processEvent(record);
+
+  // shouldn't respond with error, else
+  // event will be replayed, potentially sending multiple emails
+  // to each recipient, when multiple recipients are passed.
+  // FIX: could have an intermediary process that listens to the DynamoDB stream
+  // and posts individual messages to an SNS topic (one for each recipient). The email
+  // sender would subscribe to that topic.
   callback(null, {});
 };
